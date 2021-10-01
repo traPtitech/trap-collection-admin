aae33b7583a51a9619cd615b2f96c2022268a1d2
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2021-10-01 19:55:25 +0900

    :gear:replace compression lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
 import vue from '@vitejs/plugin-vue'
 import https from 'https'
 import path from 'path'
+import brotli from 'rollup-plugin-brotli'
 import { defineConfig } from 'vite'
-import compress from 'vite-plugin-compress'
 import WindiCSS from 'vite-plugin-windicss'
 
 const keepAliveAgent = new https.Agent({ keepAlive: true })
 const DEV_SERVER_PROXY_HOST = 'https://collection-dev.tokyotech.org'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    compress(), // 現状brotli圧縮したやつしか配信しない
-    WindiCSS()
-  ],
+  plugins: [vue(), brotli(), WindiCSS()],
   resolve: {
     alias: {
       '/@': path.resolve(__dirname, '/src').replace(/\\/g, '/')
