2324ca09e60c52c851adc472dc5573290b8726e9
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-10-06 16:38:07 +0900

    add Game info icon

--- src/components/List/Game.vue
+++ src/components/List/Game.vue
@@ -1,39 +1,63 @@
 <script lang="ts" setup>
 import InfoRound from '@vicons/material/InfoRound'
-import { NIcon, NTable, NButton, NDataTable } from 'naive-ui'
+import { NIcon, NButton, NDataTable } from 'naive-ui'
+import { TableColumns } from 'naive-ui/es/data-table/src/interface'
+import { h } from 'vue'
+
 import { GameInfo } from '/@/lib/apis'
 
 const props = defineProps<{
   games?: GameInfo[]
 }>()
 
-const columns = [
+const columns: TableColumns<GameInfo> = [
   {
     title: '',
-    key: 'info'
+    key: 'info',
+    render: () =>
+      h(
+        NButton,
+        {
+          tertiary: true
+        },
+        {
+          icon: () =>
+            h(
+              NIcon,
+              {},
+              {
+                default: () => h(InfoRound)
+              }
+            ),
+          default: () => '詳細'
+        }
+      ),
+    width: 120
   },
   {
     title: 'id',
-    key: 'id'
+    key: 'id',
+    width: 320
   },
   {
     title: 'name',
-    key: 'name'
+    key: 'name',
+    width: 250,
+    ellipsis: true
   },
   {
     title: 'createdAt',
-    key: 'createdAt'
+    key: 'createdAt',
+    width: 220
   },
   {
     title: 'descripstion',
-    key: 'description'
+    key: 'description',
+    ellipsis: true
   }
 ]
 </script>
 
 <template>
-  <NDataTable
-  :columns="columns"
-    :data="props.games"
-    />
+  <NDataTable :columns="columns" :data="props.games" />
 </template>
