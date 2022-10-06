100a3a76a3c3362086a3cc8b7d304f6ef687cba0
Sotatsu57 <tassei.moriyama@gmail.com>
2022-10-06 12:02:47 +0900

    replace NDataTable

--- src/components/List/Game.vue
+++ src/components/List/Game.vue
@@ -1,41 +1,39 @@
 <script lang="ts" setup>
 import InfoRound from '@vicons/material/InfoRound'
-import { NIcon, NTable, NButton } from 'naive-ui'
-
+import { NIcon, NTable, NButton, NDataTable } from 'naive-ui'
 import { GameInfo } from '/@/lib/apis'
 
 const props = defineProps<{
   games?: GameInfo[]
 }>()
+
+const columns = [
+  {
+    title: '',
+    key: 'info'
+  },
+  {
+    title: 'id',
+    key: 'id'
+  },
+  {
+    title: 'name',
+    key: 'name'
+  },
+  {
+    title: 'createdAt',
+    key: 'createdAt'
+  },
+  {
+    title: 'descripstion',
+    key: 'description'
+  }
+]
 </script>
+
 <template>
-  <NTable>
-    <thead>
-      <tr>
-        <th></th>
-        <th>id</th>
-        <th>name</th>
-        <th>createdAt</th>
-        <th>description</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="game in props.games" :key="game.id">
-        <th>
-          <NButton tertiary>
-            <template #icon>
-              <NIcon>
-                <InfoRound />
-              </NIcon>
-            </template>
-            詳細
-          </NButton>
-        </th>
-        <th>{{ game.id }}</th>
-        <th>{{ game.name }}</th>
-        <th>{{ game.createdAt }}</th>
-        <th>{{ game.description }}</th>
-      </tr>
-    </tbody>
-  </NTable>
+  <NDataTable
+  :columns="columns"
+    :data="props.games"
+    />
 </template>
